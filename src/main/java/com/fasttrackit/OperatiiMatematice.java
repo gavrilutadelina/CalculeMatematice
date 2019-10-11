cd341ca2077819eedf12f6c8341406edbd1731db
Adelina <gvr.adelina@yahoo.com>
2019-10-11 14:58:18 +0300

    set-up basic project structure

--- src/main/java/com/fasttrackit/OperatiiMatematice.java
+++ src/main/java/com/fasttrackit/OperatiiMatematice.java
@@ -7,8 +7,18 @@ public class OperatiiMatematice {
         return sum;
     }
 
+    public int adunare(int nr1, int nr2) {
+        int sum = nr1 + nr2;
+        return sum;
+    }
+
     public double scadere(double nr1, double nr2) {
         return (nr1 - nr2);
+    }
+
+    public int scadere(int nr1, int nr2) {
+        int dif = nr1 - nr2;
+        return dif;
     }
 
     public double inmultire(double nr1, double nr2) {
@@ -16,8 +26,18 @@ public class OperatiiMatematice {
         return inm;
     }
 
+    public int inmultire(int nr1, int nr2) {
+        int inm = nr1 * nr2;
+        return inm;
+    }
+
     public double impartire(double nr1, double nr2) {
         double imp = nr1 / nr2;
+        return imp;
+    }
+
+    public int impartire(int nr1, int nr2) {
+        int imp = nr1 / nr2;
         return imp;
     }
 }
